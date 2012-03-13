158728e9df082b866822c7fba7a95c846ffe1335
Daniel Aronne <earonne@gmail.com>
2012-03-13 00:44:45 -0400

    some packaging work here

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='nadb',
+      version='0.1',
+      description='A Django blog application',
+      author='Daniel Aronne',
+      author_email='earonne@quxisto.com',
+      url='https://github.com/earonne/nadb/',
+      packages=['nadb'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
