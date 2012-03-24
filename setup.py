5f9fb77b2135d1963b79f495207fd255572a722f
Daniel Aronne <earonne@gmail.com>
2012-03-24 13:32:22 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from distutils.core import setup
 
 setup(name='nadb',
-      version='0.1',
-      description='A Django blog application',
+      version='0.1.1',
+      license='BSD',
+      
+      description='A Django blog app',
+      long_description=open('README.rst').read(),
+      
       author='Daniel Aronne',
       author_email='earonne@quxisto.com',
       url='https://github.com/earonne/nadb/',
+      
+      install_requires = [
+          'markdown',
+      ],
+      
       packages=['nadb'],
       classifiers=['Development Status :: 3 - Alpha',
                    'Environment :: Web Environment',
